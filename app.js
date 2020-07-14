354093bc94bc260e9aa4cc19e36ef9301b219b86
Eliam LOTONGA <e.m.lotonga@gmail.com>
2020-07-14 17:56:12 +0200

    Fix bug on delete comment

--- app.js
+++ app.js
@@ -59,10 +59,11 @@ customElements.define("confirm-modal", ConfirmModal);
 
 class Comment extends HTMLElement
 {
-    constructor( owner, content, likes, added_since, id=null)
+    constructor( owner, content, likes, added_since, id, isParent=null)
     {
         super();
-        this.shadowRoot
+        this.setAttribute("class", "comment_element");
+        this.setAttribute("id", id);
         let comment = document.createElement("DIV");
         comment.setAttribute("class", "comment");
         let comment_container = document.createElement("DIV");
@@ -114,13 +115,14 @@ class Comment extends HTMLElement
             "Comment delete icon",
             svg_icon,
             "comment_delete_btn",
-            "Supprimer"
+            "Supprimer",
+            id
         );
         delete_action.addEventListener("click", ()=>
         {
             // display confirm modal
             document.querySelector("#custom_element_confirm_modal .confirm_delete").classList.add("show");
-            let el = get_node(delete_action, "comment_element");
+            let el = document.getElementById(delete_action.dataset.comment);
             ACTION_COMMENT.comment_to_delete = el;
         });
         
@@ -150,8 +152,7 @@ class Comment extends HTMLElement
             }
         });
         comment_container.querySelector(".comment_content_footer .form").appendChild(add_comment);
-        this.setAttribute("class", "comment_element");
-        if(id)// child comment doesn't have an id
+        if(isParent)// child comment doesn't have an id
         {
             let comment_icon_svg = `<svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="comments" class="svg-inline--fa fa-comments fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M532 386.2c27.5-27.1 44-61.1 44-98.2 0-80-76.5-146.1-176.2-157.9C368.3 72.5 294.3 32 208 32 93.1 32 0 103.6 0 192c0 37 16.5 71 44 98.2-15.3 30.7-37.3 54.5-37.7 54.9-6.3 6.7-8.1 16.5-4.4 25 3.6 8.5 12 14 21.2 14 53.5 0 96.7-20.2 125.2-38.8 9.2 2.1 18.7 3.7 28.4 4.9C208.1 407.6 281.8 448 368 448c20.8 0 40.8-2.4 59.8-6.8C456.3 459.7 499.4 480 553 480c9.2 0 17.5-5.5 21.2-14 3.6-8.5 1.9-18.3-4.4-25-.4-.3-22.5-24.1-37.8-54.8zm-392.8-92.3L122.1 305c-14.1 9.1-28.5 16.3-43.1 21.4 2.7-4.7 5.4-9.7 8-14.8l15.5-31.1L77.7 256C64.2 242.6 48 220.7 48 192c0-60.7 73.3-112 160-112s160 51.3 160 112-73.3 112-160 112c-16.5 0-33-1.9-49-5.6l-19.8-4.5zM498.3 352l-24.7 24.4 15.5 31.1c2.6 5.1 5.3 10.1 8 14.8-14.6-5.1-29-12.3-43.1-21.4l-17.1-11.1-19.9 4.6c-16 3.7-32.5 5.6-49 5.6-54 0-102.2-20.1-131.3-49.7C338 339.5 416 272.9 416 192c0-3.4-.4-6.7-.7-10C479.7 196.5 528 238.8 528 288c0 28.7-16.2 50.6-29.7 64z"></path></svg>`
             let comment_icon = document.createElement("DIV");
@@ -162,7 +163,6 @@ class Comment extends HTMLElement
             comment_container.appendChild(children_container);
             comment.appendChild(comment_icon);
             comment.appendChild(comment_container);
-            comment.setAttribute("id", id);
             this.appendChild(comment);
         }
         else
@@ -170,7 +170,7 @@ class Comment extends HTMLElement
             // add show children button action
             comment_container.setAttribute("class", "");
             comment_container.querySelector(".comment_content").classList.add("comment_content_child");
-            this.setAttribute("class", "comment_element comment_child");
+            this.classList.add("comment_child");
             this.appendChild(comment_container);
         }
     }
@@ -184,10 +184,11 @@ class Comment extends HTMLElement
     }
     
 }
-function createFooterBtnAction( classes, icon_classes, title, svg, span_classes, span_text )
+function createFooterBtnAction( classes, icon_classes, title, svg, span_classes, span_text, id=null )
 {
     let el = document.createElement("DIV");
     el.setAttribute("class", classes);
+    if(id) el.setAttribute("data-comment", id);
     let el_icon = document.createElement("DIV");
     el_icon.setAttribute("class", icon_classes)
     el_icon.setAttribute("title", title)
@@ -340,7 +341,8 @@ let c = new Comment(
     content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, necessitatibus.",
     likes=5,
     added_since="Il y a 2 semaines",
-    id="first_comment_johnd_123");
+    id="first_comment_johnd_123",
+    isParent=true);
 document.querySelector(".container .content").appendChild(c);
 
 let c2 = new Comment(
@@ -348,7 +350,7 @@ let c2 = new Comment(
     content="Lorem ipsum dolor sit amet consectetur.",
     likes=10,
     added_since="Il y a 1 mois",
-    id = null);
+    id = "childOffirst_comment_johnd_123");
 
 let children_container = document.querySelector("#first_comment_johnd_123 .comments_children_container");
 children_container.appendChild(c2)
@@ -359,7 +361,8 @@ let c3 = new Comment(
     content="Lorem ipsum dolor sit amet consectetur.<br/>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, necessitatibus.",
     likes=45,
     added_since="Il y a 1 minute",
-    id="second_comment_marcd_1234");
+    id="second_comment_marcd_1234",
+    isParent=true);
 document.querySelector(".container .content").appendChild(c3);
 
 
