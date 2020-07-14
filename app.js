dfa70e6e9c6dad79b65f73c3fd913e563bc17da8
Eliam LOTONGA <e.m.lotonga@gmail.com>
2020-07-14 15:37:11 +0200

    Manage: show hide msg and display child comment

--- app.js
+++ app.js
@@ -94,8 +94,11 @@ class Comment extends HTMLElement
         );
         response_action.addEventListener("click", function()
         {
-            this.parentElement.nextElementSibling.classList.toggle("show");
+            let target_node = get_node(this, "comment_element", "comment_child");
+            if(!target_node.classList.contains("show"))
+                target_node.classList.add("show");
             this.parentElement.nextElementSibling.querySelector(".new_comment").focus();
+            hide_show_child_comment_text(this)
         });
         svg_icon = `<svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="star" class="svg-inline--fa fa-star fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M528.1 171.5L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6zM388.6 312.3l23.7 138.4L288 385.4l-124.3 65.3 23.7-138.4-100.6-98 139-20.2 62.2-126 62.2 126 139 20.2-100.6 98z"></path></svg>`
         let vote_action = createFooterBtnAction(
@@ -119,7 +122,7 @@ class Comment extends HTMLElement
         {
             // display confirm modal
             document.querySelector("#custom_element_confirm_modal .confirm_delete").classList.add("show");
-            let el = get_parent_comment(delete_action, "comment_element");
+            let el = get_node(delete_action, "comment_element");
             ACTION_COMMENT.comment_to_delete = el;
         });
         
@@ -133,11 +136,15 @@ class Comment extends HTMLElement
         {
             e.preventDefault();
             if (!((e.ctrlKey  &&  e.keyCode === 13) || (e.shiftKey  &&  e.keyCode === 13)) && e.keyCode === 13) {
-                add_child_comment( add_comment.querySelector(".new_comment") );
-                add_comment.parentElement.classList.toggle("show");
-                let child_container = add_comment.parentElement.parentElement.parentElement.nextElementSibling;
+                let child_container = get_node(add_comment, "comments_children_container");
+                let new_comment = add_comment.querySelector(".new_comment");
                 if(!child_container.classList.contains("show"))
                     child_container.classList.add("show");
+                if(new_comment.value.trim() !== "")
+                {
+                    add_comment.parentElement.classList.toggle("show");
+                    add_child_comment( new_comment );
+                }
             }
         });
         comment_container.querySelector(".comment_content_footer .form").appendChild(add_comment);
@@ -182,7 +189,10 @@ function createFooterBtnAction( classes, icon_classes, title, svg, span_classes,
     let el_icon = document.createElement("DIV");
     el_icon.setAttribute("class", icon_classes)
     el_icon.setAttribute("title", title)
-    el_icon.innerHTML = svg
+    if(Array.isArray(svg))
+        el_icon.innerHTML = svg.join(" ")
+    else
+        el_icon.innerHTML = svg
     let el_span = document.createElement("SPAN");
     el_span.setAttribute("class", span_classes);
     el_span.innerHTML = span_text;
@@ -194,18 +204,20 @@ function createFooterBtnAction( classes, icon_classes, title, svg, span_classes,
 }
 function hide_or_add_show_children_btn(parent_comment=null)
 {
-    let svg_icon = `<svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="eye" class="svg-inline--fa fa-eye fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M288 144a110.94 110.94 0 0 0-31.24 5 55.4 55.4 0 0 1 7.24 27 56 56 0 0 1-56 56 55.4 55.4 0 0 1-27-7.24A111.71 111.71 0 1 0 288 144zm284.52 97.4C518.29 135.59 410.93 64 288 64S57.68 135.64 3.48 241.41a32.35 32.35 0 0 0 0 29.19C57.71 376.41 165.07 448 288 448s230.32-71.64 284.52-177.41a32.35 32.35 0 0 0 0-29.19zM288 400c-98.65 0-189.09-55-237.93-144C98.91 167 189.34 112 288 112s189.09 55 237.93 144C477.1 345 386.66 400 288 400z"></path></svg>`
+    let svg_icon_show = `<svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="eye" class="show svg-inline--fa fa-eye fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M288 144a110.94 110.94 0 0 0-31.24 5 55.4 55.4 0 0 1 7.24 27 56 56 0 0 1-56 56 55.4 55.4 0 0 1-27-7.24A111.71 111.71 0 1 0 288 144zm284.52 97.4C518.29 135.59 410.93 64 288 64S57.68 135.64 3.48 241.41a32.35 32.35 0 0 0 0 29.19C57.71 376.41 165.07 448 288 448s230.32-71.64 284.52-177.41a32.35 32.35 0 0 0 0-29.19zM288 400c-98.65 0-189.09-55-237.93-144C98.91 167 189.34 112 288 112s189.09 55 237.93 144C477.1 345 386.66 400 288 400z"></path></svg>`
+    let svg_icon_hide = `<svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="eye-slash" class="hide svg-inline--fa fa-eye-slash fa-w-20" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path fill="currentColor" d="M634 471L36 3.51A16 16 0 0 0 13.51 6l-10 12.49A16 16 0 0 0 6 41l598 467.49a16 16 0 0 0 22.49-2.49l10-12.49A16 16 0 0 0 634 471zM296.79 146.47l134.79 105.38C429.36 191.91 380.48 144 320 144a112.26 112.26 0 0 0-23.21 2.47zm46.42 219.07L208.42 260.16C210.65 320.09 259.53 368 320 368a113 113 0 0 0 23.21-2.46zM320 112c98.65 0 189.09 55 237.93 144a285.53 285.53 0 0 1-44 60.2l37.74 29.5a333.7 333.7 0 0 0 52.9-75.11 32.35 32.35 0 0 0 0-29.19C550.29 135.59 442.93 64 320 64c-36.7 0-71.71 7-104.63 18.81l46.41 36.29c18.94-4.3 38.34-7.1 58.22-7.1zm0 288c-98.65 0-189.08-55-237.93-144a285.47 285.47 0 0 1 44.05-60.19l-37.74-29.5a333.6 333.6 0 0 0-52.89 75.1 32.35 32.35 0 0 0 0 29.19C89.72 376.41 197.08 448 320 448c36.7 0 71.71-7.05 104.63-18.81l-46.41-36.28C359.28 397.2 339.89 400 320 400z"></path></svg>`
     let children_action = createFooterBtnAction(
         "comment_actions comment_show_children_action",
         "icon comment_show_children_icon",
         "Comment show children icon",
-        svg_icon,
+        [svg_icon_show, svg_icon_hide],
         "comment_show_children_btn",
         "Afficher les réponses"
     );
     children_action.addEventListener("click", function()
     {
-        this.parentElement.parentElement.parentElement.nextElementSibling.classList.toggle("show");
+        get_node(this.parentElement, "comment_element", "comment_child").classList.toggle("show");
+        hide_show_child_comment_text(this);
     });
 
     if(parent_comment)
@@ -242,10 +254,12 @@ function hide_or_add_show_children_btn(parent_comment=null)
                     comment_children_container.querySelector(".actions .comment_show_children_action")
                 )
             }
+            hide_show_child_comment_text(comments_children_container);
         })
     }
 }
-
+/****************  Add parent Comment  ****************
+ ******************************************************/
 let add_parent_comment = document.querySelector("form.add_parent_comment");
 add_parent_comment.addEventListener("submit", (e)=>
 {
@@ -268,7 +282,8 @@ add_parent_comment.addEventListener("submit", (e)=>
         // TODO INSERT INTO DATABASE THE CURRENT COMMENT CHILD
     }
 });
-
+/****************  Add child Comment  ****************
+ ******************************************************/
 function add_child_comment(el)
 {
     // TODO Only for dev env
@@ -277,7 +292,8 @@ function add_child_comment(el)
     
     if(el.value.trim() !== "")
     {
-        let parent_el = this.get_parent_comment(el).querySelector(".comments_children_container");
+        //let parent_el = this.get_node(el).querySelector(".comments_children_container");
+        let parent_el = this.get_node(el, "comments_children_container");
         let c = new Comment(
             owner= "Marc DOE",
             content=el.value,
@@ -285,23 +301,36 @@ function add_child_comment(el)
             added_since=random_since_date,
             id = null );
         parent_el.prepend(c);
-        hide_or_add_show_children_btn(parent_el.parentElement);
         el.value = "";
+        hide_or_add_show_children_btn(parent_el.parentElement);
         // TODO INSERT INTO DATABASE THE CURRENT COMMENT CHILD
     } 
 }
-function get_parent_comment(el, identifier_class=undefined)
+/****************  Get parentNode  ****************
+ ******************************************************/
+function get_node(el, class_name=null, not=null)
 {
-    let identifier = identifier_class?identifier_class :"comment_container";
-    let tmp_parent = el.parentElement;
-    if(tmp_parent.classList.contains(identifier))
-    {
-        return tmp_parent;
-    }
-    else
-    return get_parent_comment(tmp_parent, identifier);
+    class_name = class_name?class_name :"comment_container";
+    let selector = not !== null?`.${class_name}:not(.${not})`: `.${class_name}`;
+    let childElement = el.querySelector(selector);
+    if(el.classList.contains(class_name) && !el.classList.contains(not))
+        return el;
+    else if(childElement)
+        return childElement;
+    return get_node(el.parentElement, class_name, not);
 }
 
+/****************  Manage hide/show child comment text  ****************
+ ******************************************************/
+function hide_show_child_comment_text(el)
+{
+    let node = get_node(el, "comment_element", "comment_child");
+    let target_node = get_node(el, "comment_show_children_btn");
+    if(node.classList.contains("show"))
+        target_node.innerText = "Masquer les réponses";
+    else
+        target_node.innerText = "afficher les réponses";
+}
 
 customElements.define("comment-element", Comment)
 let c = new Comment(
