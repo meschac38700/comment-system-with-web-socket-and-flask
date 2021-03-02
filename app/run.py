3b3b050aab966033a3df065b09632bd1c45843e8
Eliam LOTONGA <e.m.lotonga@gmail.com>
2021-03-02 12:06:30 +0100

    Fix import

--- app/run.py
+++ app/run.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
 from flask_socketio import SocketIO, emit
 from flask_cors import CORS
 from app.models import Database
